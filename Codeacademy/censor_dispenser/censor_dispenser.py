2ecee5e519c9f56b9847dc1422cee5fd40f97d4c
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-05-19 13:20:06 -0400

    Add phrase remover (excercise 2)

--- Codeacademy/censor_dispenser/censor_dispenser.py
+++ Codeacademy/censor_dispenser/censor_dispenser.py
@@ -10,6 +10,8 @@ proprietary_terms = ["she", "personality matrix", "sense of self", "self-preserv
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
 #Write a function that can censor a specific word or phrase from a body of text, and then return the text.
+#Mr. Cloudy has asked you to use the function to censor all instances of the phrase learning algorithms from the first email, email_one.
+#Mr. Cloudy doesn’t care how you censor it, he just wants it done.
 
 def case_shuffler(phrase):
     all_cases = [phrase.upper(),phrase.lower(),phrase.title()]
@@ -29,14 +31,15 @@ def basic_remover(phrase,text):
             phrase_instance = text.find(phrase)
     return text
 
-print(basic_remover("we",email_one))
-
-#Mr. Cloudy has asked you to use the function to censor all instances of the phrase learning algorithms from the first email, email_one.
-#Mr. Cloudy doesn’t care how you censor it, he just wants it done.
-
 #Write a function that can censor not just a specific word or phrase from a body of text, but a whole list of words and phrases, and then return the text.
 #Mr. Cloudy has asked that you censor all words and phrases from the following list in email_two. (Reference prorietary_terms)
 
+def list_remover(phrases,text):
+    for phrase in phrases:
+        text = basic_remover(phrase,text)
+    return text 
+
+print(list_remover(proprietary_terms,email_two))
 
 #The most recent email update has concerned Mr. Cloudy, but not for the reasons you might think. He tells you, “this is too alarmist for the Board of Investors! Let’s tone down the negative language and remove unnecessary instances of ‘negative words.’”
 #Write a function that can censor any occurance of a word from the “negative words” list after any “negative” word has occurred twice, as well as censoring everything from the list from the previous step as well and use it to censor email_three. (reference negative_words)
